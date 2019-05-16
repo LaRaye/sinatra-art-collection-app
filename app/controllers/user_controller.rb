9d32bfe31e4fa689f90523fa4f0a95142736ca31
LaRaye <lnj2105@gmail.com>
2019-05-16 01:53:28 -0400

    login route/from

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -19,7 +19,14 @@ class UserController < ApplicationController
   end
 
   post '/login' do
+    @user = User.find_by(:username => params[:username])
 
-  end 
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/' ###
+    else
+      redirect '/login' ###create failure
+    end
+  end
 
 end

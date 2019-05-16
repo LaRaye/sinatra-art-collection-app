725955e4847df19c4001dc9e8a9786b13a7e7bd5
LaRaye <lnj2105@gmail.com>
2019-05-16 15:36:05 -0400

    paintings routes

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,11 +1,21 @@
 class PaintingsController < ApplicationController
   get '/paintings' do
+    # Helpers.redirect_if_not_logged_in(session)
     @paintings = Painting.all
     erb :'paintings/index'
   end
 
+  get '/paintings/new' do
+    erb :'paintings/new'
+  end
 
-
+  post '/paintings' do
+    if params[:name] != ""
+      @painting = Painting.create(params)
+    end
+    binding.pry
+    erb :'paintings/index'
+  end
 
 
 
--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,4 +1,4 @@
-class Helpers
+class Helpers < ApplicationController
   def self.current_user(session)
     User.find(session[:user_id])
   end
@@ -6,4 +6,10 @@ class Helpers
   def self.is_logged_in?(session)
     !!session[:user_id]
   end
+
+  # def self.redirect_if_not_logged_in(session)
+  #   if Helpers.is_logged_in?(session) == false
+  #     redirect'/'
+  #   end
+  # end
 end
